f152b0146ae630f13ee59bff3670412ec2d9be72
Moïse Gui <k.moise.gui@gmail.com>
2021-09-09 22:57:13 +0100

    react-dotenv integrated

---
+++ public/env.js
@@ -0,0 +1,8 @@
+window.env = {
+  "FIREBASE_API_KEY": "AIzaSyBbhi_hCEqedAMezxHn8pZfESqLE0HT4IE",
+  "FIREBASE_AUTH_DOMAIN": "linkedin-clone-moise.firebaseapp.com",
+  "FIREBASE_PROJECT_ID": "linkedin-clone-moise",
+  "FIREBASE_STORAGE_BUCKET": "linkedin-clone-moise.appspot.com",
+  "FIREBASE_MESSEGING_SENDER_ID": "470787678509",
+  "FIREBASE_APP_ID": "1:470787678509:web:3750a69aa0ab4b504673a4"
+};
--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,27 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import env from "react-dotenv";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBbhi_hCEqedAMezxHn8pZfESqLE0HT4IE",
+//     authDomain: "linkedin-clone-moise.firebaseapp.com",
+//     projectId: "linkedin-clone-moise",
+//     storageBucket: "linkedin-clone-moise.appspot.com",
+//     messagingSenderId: "470787678509",
+//     appId: "1:470787678509:web:3750a69aa0ab4b504673a4"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyBbhi_hCEqedAMezxHn8pZfESqLE0HT4IE",
-    authDomain: "linkedin-clone-moise.firebaseapp.com",
-    projectId: "linkedin-clone-moise",
-    storageBucket: "linkedin-clone-moise.appspot.com",
-    messagingSenderId: "470787678509",
-    appId: "1:470787678509:web:3750a69aa0ab4b504673a4"
+    apiKey: env.FIREBASE_API_KEY,
+    authDomain: env.FIREBASE_AUTH_DOMAIN,
+    projectId: env.FIREBASE_PROJECT_ID,
+    storageBucket: env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.FIREBASE_MESSEGING_SENDER_ID,
+    appId: env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
